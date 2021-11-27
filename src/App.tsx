c6f0521afe55cdc99cb24c0d7364a919ab1c23fa
Sebastian Forman <sebastian@forman.no>
2021-11-27 15:01:01 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <>Its working</>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            "@babel/preset-env",
+            [
+              "@babel/preset-react",
+              {
+                runtime: "automatic",
+              },
+            ],
+            "@babel/preset-typescript",
+          ],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        loader: "file-loader",
+        options: {
+          name: "/assets/img/[name].[ext]",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    historyApiFallback: true,
+    static: path.resolve(__dirname, "./dist"),
+    hot: true,
+  },
+};
